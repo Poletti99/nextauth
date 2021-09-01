3665a2cb8c5dbbd916e2c6d70bab20123bd34d75
Victor Poletti <victorpoletti2009@hotmail.com>
2021-09-01 07:42:33 -0300

    Aplicação explicando autenticação

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,38 @@
+import { useContext, useEffect } from 'react';
+import { Can } from '../components/Can';
+import { AuthContext } from '../contexts/AuthContext';
+import { setupAPIClient } from '../services/api';
+import { api } from '../services/apiClient';
+import { withSSRAuth } from '../utils/withSSRAuth';
+
+export default function Dashboard() {
+  const { user, signOut } = useContext(AuthContext);
+
+  useEffect(() => {
+    api
+      .get('/me')
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error, 'error'));
+  }, []);
+
+  return (
+    <>
+      <h1>Aooba {user?.email}</h1>
+      <button onClick={signOut}>Sign Out</button>
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
+    </>
+  );
+}
+
+export const getServerSideProps = withSSRAuth(async (context) => {
+  const apiClient = setupAPIClient(context);
+  const response = await apiClient.get('/me');
+
+  console.log(response.data);
+
+  return {
+    props: {},
+  };
+});
---
+++ src/services/api.ts
@@ -0,0 +1,102 @@
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
+import { signOut } from '../contexts/AuthContext';
+import { AuthTokenError } from './errors/AuthTokenError';
+
+let isRefreshing = false;
+let failedRequestQueue = [];
+
+export function setupAPIClient(context = undefined) {
+  let cookies = parseCookies(context);
+
+  const api = axios.create({
+    baseURL: 'http://localhost:3333',
+    headers: {
+      Authorization: `Bearer ${cookies['nextauth.token']}`,
+    },
+  });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+      if (error.response?.status === 401) {
+        if (error.response?.data?.code === 'token.expired') {
+          cookies = parseCookies(context);
+
+          const { 'nextauth.refreshToken': refreshToken } = cookies;
+          const originalConfig = error.config;
+
+          if (!isRefreshing) {
+            isRefreshing = true;
+
+            api
+              .post('/refresh', {
+                refreshToken,
+              })
+              .then((response) => {
+                const { token } = response.data;
+
+                setCookie(context, 'nextauth.token', token, {
+                  maxAge: 60 * 60 * 24 * 30, // 30 dias
+                  path: '/',
+                });
+
+                setCookie(
+                  context,
+                  'nextauth.refreshToken',
+                  response.data.refreshToken,
+                  {
+                    maxAge: 60 * 60 * 24 * 30, // 30 dias
+                    path: '/',
+                  },
+                );
+
+                api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+                failedRequestQueue.forEach((request) =>
+                  request.onSuccess(token),
+                );
+                failedRequestQueue = [];
+              })
+              .catch((error) => {
+                failedRequestQueue.forEach((request) =>
+                  request.onFailure(error),
+                );
+                failedRequestQueue = [];
+
+                if (process.browser) {
+                  signOut();
+                }
+              })
+              .finally(() => {
+                isRefreshing = false;
+              });
+          }
+
+          return new Promise((resolve, reject) => {
+            failedRequestQueue.push({
+              onSuccess: (token: string) => {
+                originalConfig.headers['Authorization'] = `Bearer ${token}`;
+
+                resolve(api(originalConfig));
+              },
+              onFailure: (error: AxiosError) => {
+                reject(error);
+              },
+            });
+          });
+        } else {
+          if (process.browser) {
+            signOut();
+          } else {
+            return Promise.reject(new AuthTokenError());
+          }
+        }
+      }
+
+      return Promise.reject(error);
+    },
+  );
+
+  return api;
+}
